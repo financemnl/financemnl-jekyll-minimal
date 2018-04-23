b0713abb07339e6cc00d3ac932a4dd759e72f374
Niko Jaro <niko.jaro@paymaya.com>
2018-04-23 17:30:25 +0800

    Fix headers

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 ---
 layout: null
 ---
-
+// Cache name: adjust version number to invalidate service worker cachce.
+var CACHE_NAME = 'financemnl-v2';
 var urlsToCache = [];
 
 // Cache posts
@@ -34,16 +35,24 @@ var urlsToCache = [];
     {% endif %}
 {% endfor %}
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      cache.match(event.request).then(function(response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      }).catch(function() {
-        // If it can't fetch the asset, display the offline only page
-        return caches.match('/offline.html')
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    cache.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  );
+});
