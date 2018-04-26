2fc0b427684358f0f2764b2687df01f143b1ce8a
Niko Jaro <niko.jaro@paymaya.com>
2018-04-26 12:31:20 +0800

    Critical CSS

--- _critical.js
+++ _critical.js
@@ -4,14 +4,14 @@ var criticalcss = require("criticalcss");
 var fs = require('fs');
 var tmpDir = require('os').tmpdir();
 
-var cssUrl = 'http://localhost:4000/assets/css/main.css';
+var cssUrl = 'https://financemnl.com/assets/css/main.css';
 var cssPath = path.join( tmpDir, 'main.css' );
 request(cssUrl).pipe(fs.createWriteStream(cssPath)).on('close', function() {
   criticalcss.getRules(cssPath, function(err, output) {
     if (err) {
       throw new Error(err);
     } else {
-      criticalcss.findCritical("http://localhost:4000/", { rules: JSON.parse(output) }, function(err, output) {
+      criticalcss.findCritical("https://financemnl.com/", { rules: JSON.parse(output) }, function(err, output) {
         if (err) {
           throw new Error(err);
         } else {
