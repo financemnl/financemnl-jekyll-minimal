53f29dfb38570a7513604207526ce12796ef36ae
Niko Jaro <niko.jaro@paymaya.com>
2018-04-28 00:26:08 +0800

    Service Worker

---
+++ _pages/offline.md
@@ -0,0 +1,9 @@
+---
+title: You're Offline
+layout: single
+author_profile: false
+sitemap: false
+permalink: "/offline/"
+---
+
+You are offline.
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+---
+layout: null
+---
+
+// Cache name: adjust version number to invalidate service worker cachce.
+var CACHE_NAME = 'james-ives-cache-v2';
+var urlsToCache = [];
+
+// Cache assets
+{% for asset in site.static_files %}
+    {% if asset.path contains '/assets/images' or asset.path contains '/assets/posts' or asset.extname == '.js' %}
+    urlsToCache.push("{{ file.path }}")
+    {% endif %}
+{% endfor %}
+
+// Cache posts
+{% for post in site.posts %}
+  urlsToCache.push("{{ post.url }}")
+{% endfor %}
+
+// Cache pages
+{% for page in site.html_pages %}
+  urlsToCache.push("{{ page.url }}")
+{% endfor %}
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    cache.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      cache.match(event.request).then(function(response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(function() {
+        // If it can't fetch the asset, display the offline only page
+        return caches.match('/offline')
+      })
+    );
+  });
