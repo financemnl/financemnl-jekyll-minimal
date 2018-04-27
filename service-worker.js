d3ee476f82a64bf5b79116281e0dfe1eb4cad7eb
Niko Jaro <niko.jaro@paymaya.com>
2018-04-28 01:02:47 +0800

    Manual Cache

--- service-worker.js
+++ service-worker.js
@@ -2,26 +2,35 @@
 layout: null
 ---
 
-var urlsToCache = [];
+var urlsToCache = [
+  // Cache posts
+  {% for post in site.posts %}
+    urlsToCache.push("{{ post.url }}")
+  {% endfor %}
+
+  // Cache pages
+  {% for page in site.html_pages %}
+    urlsToCache.push("{{ page.url }}")
+  {% endfor %}
+
+  "/assets/css/main.css",
+  "/assets/js/main.min.css",
+  "/assets/css/critical.css",
+  "/assets/js/loadCSS.js",
+  "/about/",
+  "/index.html"
+];
 // Cache name: adjust version number to invalidate service worker cachce.
 var CACHE_NAME = 'james-ives-cache-v2';
 
 // Cache assets
-{% for asset in site.static_files %}
+{% for asset in site.asset %}
     {% if asset.path contains '://res.cloudinary.com' or asset.extname == '.css' or asset.extname == '.js' %}
     urlsToCache.push("{{ file.path }}")
     {% endif %}
 {% endfor %}
 
-// Cache posts
-{% for post in site.posts %}
-  urlsToCache.push("{{ post.url }}")
-{% endfor %}
 
-// Cache pages
-{% for page in site.html_pages %}
-  urlsToCache.push("{{ page.url }}")
-{% endfor %}
 
 self.addEventListener('install', function(event) {
   // Perform install steps
